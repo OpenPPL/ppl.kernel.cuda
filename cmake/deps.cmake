4001bdb4db8451b4465989fddda736c0fd2692fa
JieXin Liang <Alcanderian@users.noreply.github.com>
2023-08-08 14:30:42 +0800

    [fix] update hpcc& pplcommon

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -48,7 +48,7 @@ if(NOT Git_FOUND)
     message(FATAL_ERROR "git is required.")
 endif()
 
-set(__HPCC_COMMIT__ af7dcc6c1b1eaf622b3d01472b89ce62d881f66c)
+set(__HPCC_COMMIT__ e674b696eb0d982a4d5a5ea358bf2199efffe687)
 
 if(PPLNN_DEP_HPCC_PKG)
     FetchContent_Declare(hpcc
@@ -80,7 +80,7 @@ endif()
 
 set(PPLCOMMON_HOLD_DEPS ${PPLNN_HOLD_DEPS})
 
-set(__PPLCOMMON_COMMIT__ 711b74e285ae36445e50d04bffd2e1ca3c589a1c)
+set(__PPLCOMMON_COMMIT__ 95210fb43cf90f3b037c4be7439a0d20d28daca1)
 
 set(PPLCOMMON_USE_X86_64 ${PPLNN_USE_X86_64})
 set(PPLCOMMON_USE_AARCH64 ${PPLNN_USE_AARCH64})
